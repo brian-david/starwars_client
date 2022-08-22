11e1dc33bc1eef251193ba8df744c643a38271b3
Brian Irons <the.brian.irons@gmail.com>
2022-08-22 12:19:48 +0200

    added store

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
-import React from "react";
-import logo from "./logo.svg";
 import "./App.css";
+import { Provider } from "react-redux";
+import store from "./state/store";
 
 function App() {
   return (
-    <div className="App">
-      <h1>This is the home page</h1>
-    </div>
+    <Provider store={store}>
+      <div className="App">
+        <h1>This is the home page</h1>
+      </div>
+    </Provider>
   );
 }
 
