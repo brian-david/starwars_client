fc6380305c538ca951bba2562706ad541375aedf
Brian Irons <the.brian.irons@gmail.com>
2022-08-22 13:58:57 +0200

    data mapping working correctly

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,4 +1,5 @@
 import { gql, useQuery } from "@apollo/client";
+import { Person } from "../types/root";
 
 const PEOPLE_PAGE = gql`
   query GetPeoplePage {
@@ -23,7 +24,14 @@ const HomePage = () => {
   const { loading, error, data } = useQuery(PEOPLE_PAGE);
   if (loading) return <p>loading...</p>;
   if (error) return <p>`ERROR: ${error.message}`</p>;
-  return <p>{JSON.stringify(data)}</p>;
+  return (
+    <div>
+      {/* {JSON.stringify(data)} */}
+      {data.peoplepage.results.map((person: Person, index: number) => {
+        return <p>{person.name}</p>;
+      })}
+    </div>
+  );
 };
 
 export default HomePage;
