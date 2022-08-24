93c193e71b955f193a8c2c7e6c5f7224ec6a4840
Brian Irons <the.brian.irons@gmail.com>
2022-08-24 11:15:46 +0200

    Updates

---
+++ precache-manifest.a832585a383f70d495302b70c01c6789.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "a3c41af6e87688aa4a55",
+    "url": "/starwars_client/static/css/main.a60a8bd6.chunk.css"
+  },
+  {
+    "revision": "a3c41af6e87688aa4a55",
+    "url": "/starwars_client/static/js/main.a3c41af6.chunk.js"
+  },
+  {
+    "revision": "94421d0c55535ff39cc4",
+    "url": "/starwars_client/static/js/1.94421d0c.chunk.js"
+  },
+  {
+    "revision": "df51d1c9033bd62d30ba",
+    "url": "/starwars_client/static/js/2.df51d1c9.chunk.js"
+  },
+  {
+    "revision": "f3b465470a3d0b63ca8f",
+    "url": "/starwars_client/static/js/runtime~main.f3b46547.js"
+  },
+  {
+    "revision": "73616059cbe8eababce93b672c44a883",
+    "url": "/starwars_client/index.html"
+  }
+];
